cb6d2cac9a0023ac75d14991c61dfa485a3e70d4
lpp <1072709186@qq.com>
2020-09-16 08:18:31 +0800

    提交的Student类包含名字年龄性别

---
+++ Demo0916/src/com/lpp/Student.java
@@ -0,0 +1,13 @@
+package com.lpp;
+
+/**
+ * @Auther: lpp
+ * @Date: 2020/9/16 - 09 - 16 - 8:16
+ * @Description: com.lpp
+ * @version: 1.0
+ */
+public class Student {
+    private String name;
+    private int age;
+    private String sex;
+}
