a239fd428f8ce0916748fbf488b4f2e9a2fa7d51
lpp <1072709186@qq.com>
2020-09-16 08:26:47 +0800

    添加了构造方法

--- Demo0916/src/com/lpp/Student.java
+++ Demo0916/src/com/lpp/Student.java
@@ -10,4 +10,10 @@ public class Student {
     private String name;
     private int age;
     private String sex;
+
+    public Student(String name, int age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
 }
